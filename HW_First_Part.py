bd7c3d4455a5ae1162f279f2ccd9795600ed5434
AriessGit <ariecold@gmail.com>
2025-08-03 01:31:36 +0300

    Update and rename part_one.py to HW_First_Part.py

--- HW_First_Part.py
+++ HW_First_Part.py
@@ -1,8 +1,9 @@
 
+#HOMEWORK 28/07/25 PART 1 OF 2:
+
 import psycopg2
 import psycopg2.extras
 
-#HOMEWORK 28/07/25 PART 1 OF 2:
 
 ## connect to postgres server
 try:
@@ -36,13 +37,13 @@ finally:
     cur.close()
 
 
-# Creating data to insert into our table:
+#creating data to insert in a table:
 data_products = [('Laptop', 3200.50, True),
                 ('Mouse', 99.99, True),
                 ('Keyboard', 250.00, False),
                 ('Monitor', 1190.95, True)]
 
-## Inserting new data into a table and deleting other data if it exists
+## Inserting new data to create a table and deleting other data if it exists
 try:
     cur = conn.cursor()
     cur.execute("DELETE FROM products;")
@@ -55,7 +56,7 @@ except psycopg2.Error as e:
 finally:
     cur.close()
 
-# Search for relevant data and print it (Products that are in stock - True)
+#Search for relevant data and print it (Products that are in stock - True)
 try:
     cur = conn.cursor()
     cur.execute('''SELECT * FROM products WHERE in_stock = TRUE;''')
